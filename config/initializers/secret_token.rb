735ccc86bdb5316cb5b28e092bb3357c6a7eeefa
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-17 11:02:20 -0700

    Initial rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StatChater::Application.config.secret_key_base = '9f0cb1bfbed9fc277b7cef9fe838b1ec3b039aa8dd7a1ad18b4ec85ca3790837b00e7af93eaf4ee722076ffb01032cc96907cdb9f45cde28551620ca029937d7'
