28cc3359f16bc62cfb53ae40b28785697db58eef
Luke Martinez <lukemartinez@gmail.com>
2014-02-18 14:28:53 -0700

    fetching commits with commit fetcher

--- app/models/services/commit_fetcher.rb
+++ app/models/services/commit_fetcher.rb
@@ -1,15 +1,19 @@
 class CommitFetcher
-  attr_reader :events
+  attr_reader :commits
 
-  def raw_events(user)
-    response = Faraday.get('https://api.github.com/users/#{user.github_name}/events')
-    @events = JSON.parse(response.body)
+  def raw_commits(user)
+    response = Faraday.get('https://github.com/users/#{user.username}/contributions_calendar_data')
+    @commits = JSON.parse(response.body)
   end
 
-  def commits(user)
-    events.select do |hash|
-      hash["type"] == "PushEvent" && hash["payload"]["commits"][0]["author"]["email"] == user.email 
-    end
+  def commits_this_week(user)
+    commits_for_current_week.map do |summary|
+      summary[1]
+    end.inject(:+)
+  end
+
+  def commits_for_week(week_num = Date.today.cweek)
+    commits.select { |item| (Date.parse(item[0])).cweek == week_num }
   end
 
 end
