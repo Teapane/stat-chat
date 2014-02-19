2ac08e31fbf565f89f60dfa683b44ca9bc65013b
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-19 16:44:54 -0700

    fix broken specs for login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,8 +5,8 @@ describe 'login with github' do
   context 'when I visit the page without having logged in' do
     it 'has a login form' do
       visit login_path
-      within('#login_form') do
-        page.should have_link('Login with Github')
+      within('.login-button') do
+        page.should have_button('Login with Github')
       end
     end
 
