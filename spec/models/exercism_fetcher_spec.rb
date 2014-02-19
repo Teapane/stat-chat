5069c31274ab7ce594ab9a7873f3d4c65a7f506f
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-19 13:19:51 -0700

    first exercism fetcher spec

--- spec/models/exercism_fetcher_spec.rb
+++ spec/models/exercism_fetcher_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe ExercismFetcher do
-  
-  
+  #attr_reader :response, :parsed_data, :commit_fetcher
+
+  before do
+    VCR.use_cassette 'model/exercism_fetcher' do
+      @exercism_fetcher = ExercismFetcher.new
+      @response = exercism_fetcher.response
+      @parsed_data = exercism_fetcher.commits
+    end
+
+    it "gets response from exercism api" do
+     expect(response.status).to eq 200
+    end
+  end
 end
