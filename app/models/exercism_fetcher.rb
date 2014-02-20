cb25864b2e884a2ace5564570079952abd586696
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-20 11:55:08 -0700

    hibernating exercises done

--- app/models/exercism_fetcher.rb
+++ app/models/exercism_fetcher.rb
@@ -1,8 +1,13 @@
+
 class ExercismFetcher
   attr_reader :response, :stats
 
   def initialize(username)
     @response = Faraday.get("http://exercism.io/api/v1/stats/#{username}/snapshot")
     @stats = JSON.parse(response.body)
+  end
+
+  def hibernating_count
+    stats[:activity][:exercises][:hibernating].to_i
   end
 end
--- spec/models/exercism_fetcher_spec.rb
+++ spec/models/exercism_fetcher_spec.rb
@@ -12,8 +12,40 @@ describe ExercismFetcher do
     end
   end
 
+    def snapshot
+      snapshot = {
+                    activity: {
+                    most_recent: {
+                    submission: "2014-02-19T04:23:37Z",
+                    nitpick: "2014-01-15T17:45:11Z"
+                    },
+                    exercises: {
+                    active: "1",
+                    hibernating: "3",
+                    completed: "7"
+                    },
+                    total: {
+                    nitpicks: "0",
+                    submissions: "30",
+                    languages: 2
+                    },
+                    past7: {
+                    nitpicks: "0",
+                    submissions: "3",
+                    languages: 1
+                    }
+                    }
+                    }
+    end
+
     it "gets response from exercism api" do
      expect(response.status).to eq 200
+    end
+
+    it "returns hibernating exercises" do 
+      
+      exercism_fetcher.stub(:stats).and_return(snapshot)
+      expect(exercism_fetcher.hibernating_count).to eq 3
     end
   end
 
