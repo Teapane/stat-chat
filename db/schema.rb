f6a2076e98b204af914254683bd77a340511e7e1
Luke Martinez <lukemartinez@gmail.com>
2014-02-20 12:00:56 -0700

    added languages score to score model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220185441) do
+ActiveRecord::Schema.define(version: 20140220185911) do
 
   create_table "scores", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140220185441) do
     t.integer  "nitpicks_score",          default: 0
     t.integer  "submission_score",        default: 0
     t.integer  "submissions_score",       default: 0
+    t.integer  "languages_score",         default: 0
   end
 
   create_table "users", force: true do |t|
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Score do
-  
+
   it {should validate_presence_of(:commits_score)}
   it {should validate_presence_of(:public_repo_score)}
   it {should validate_presence_of(:contributed_repos_score)}
   it {should validate_presence_of(:hibernating_score)}
   it {should validate_presence_of(:nitpicks_score)}
-  it {should validate_presence_of(:submissions_score)}
+  it {should validate_presence_of(:submissions_score) }
+  it {should validate_presence_of(:languages_score)}
 end
