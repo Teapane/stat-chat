15eeb98e1f70b8f8d045c698b95620605e81ffd3
Tyler Long <tyler.stephen.long@gmail.com>
2014-02-20 11:41:37 -0700

    first spec passing/cism model

--- app/models/exercism_fetcher.rb
+++ app/models/exercism_fetcher.rb
@@ -1,2 +1,8 @@
 class ExercismFetcher
+  attr_reader :response, :stats
+
+  def initialize(username)
+    @response = Faraday.get("http://exercism.io/api/v1/stats/#{username}/snapshot")
+    @stats = JSON.parse(response.body)
+  end
 end
--- spec/models/exercism_fetcher_spec.rb
+++ spec/models/exercism_fetcher_spec.rb
@@ -1,17 +1,19 @@
-#require 'spec_helper'
+require 'spec_helper'
 
-# describe ExercismFetcher do
-#   #attr_reader :response, :parsed_data, :commit_fetcher
+describe ExercismFetcher do
+  attr_reader :response, :exercism_fetcher
 
-#   before do
-#     VCR.use_cassette 'model/exercism_fetcher' do
-#       @exercism_fetcher = ExercismFetcher.new
-#       @response = exercism_fetcher.response
-#       @parsed_data = exercism_fetcher.commits
-#     end
+  before do
+    VCR.use_cassette 'model/exercism_fetcher' do
+      username = 'BryanaKnight'
+      @exercism_fetcher = ExercismFetcher.new(username)
+      @response = exercism_fetcher.response
+      @parsed_data = exercism_fetcher.stats
+    end
+  end
 
-#     it "gets response from exercism api" do
-#      expect(response.status).to eq 200
-#     end
-#   end
-# end
+    it "gets response from exercism api" do
+     expect(response.status).to eq 200
+    end
+  end
+
