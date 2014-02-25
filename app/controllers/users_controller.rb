c4e1dd5df7e3bf91b4fa76a6deb8e1dd89398c4d
Will Mitchell <wvmitchell@gmail.com>
2014-02-25 13:01:55 -0700

    better pie chart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @commit_points = @user.commits
+    score = @user.scores.last
+    @commits_score = score.commits_score
+    @repos_score = score.public_repo_score
+    @exercism_submissions_score = score.submissions_score
     @badges = BadgeFetcher.new(@user.nickname).fetch
   end
 
